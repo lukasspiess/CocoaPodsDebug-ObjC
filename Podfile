be58e3b37abb8a35bb5a2091535688d76b549554
Lukas Spieß <lspiess@microsoft.com>
2015-06-25 13:53:08 +0200

    Switch to not using frameworks
    This works.

--- Podfile
+++ Podfile
@@ -1,6 +1,5 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '6.0'
-use_frameworks!
 
 target 'CocoaPodsDebug-ObjC' do
 	pod 'HockeySDK', '3.7.0'
--- Pods/Target Support Files/Pods-CocoaPodsDebug-ObjC/Pods-CocoaPodsDebug-ObjC-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CocoaPodsDebug_ObjCVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CocoaPodsDebug_ObjCVersionString[];
-
